2565c75d0b5d0d8ca08a4355a710b73b7e8fa944
eze1x <126312190+eze1x@users.noreply.github.com>
2026-02-15 20:39:59 -0300

    Update script.js

--- blackcore-gym/script.js
+++ blackcore-gym/script.js
@@ -1,7 +1,15 @@
 const btn = document.getElementById("verPlanesBtn");
 const plans = document.getElementById("planes");
+const planBtns = document.querySelectorAll(".plan-btn");
+const infoMsg = document.getElementById("infoMsg");
 
 btn.addEventListener("click", () => {
     plans.classList.add("show");
     plans.scrollIntoView({ behavior: "smooth" });
+});
+
+planBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        infoMsg.textContent = "✅ Gracias por tu interés. Este es un proyecto demostrativo.";
+    });
 });
